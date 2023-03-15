81669c7fcd04c5bb7bdafe6f33eab2b3e0dc6dfb
Oleksii <alex.yurchenko@gmail.com>
2023-03-16 00:37:34 +0200

    burger list via react added

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -2,17 +2,28 @@ import { Container } from "../Container/Container";
 import { Order } from "../Order/Order";
 import style from "./Catalog.module.css";
 
+const goodsList = [
+  { title: "Мясная бомба" },
+  { title: "Супер сырный" },
+  { title: "Сытный" },
+  { title: "Итальянский" },
+  { title: "Вечная классика" },
+  { title: "Тяжелый удар" },
+];
+
 export const Catalog = () => {
   return (
-    <section className="catalog">
-      <Container className="catalog__container">
+    <section className={style.catalog}>
+      <Container className={style.container}>
         <Order />
-        <div className="catalog__wrapper">
-          <h2 className="catalog__title">Бургеры</h2>
+        <div className={style.wrapper}>
+          <h2 className={style.title}>Бургеры</h2>
 
-          <div className="catalog__wrap_list">
-            <ul className="catalog__list">
-              <li className="catalog__item"></li>
+          <div className={style.wrap_list}>
+            <ul className={style.list}>
+              {goodsList.map((item) => (
+                <li className={style.item}>{item.title}</li>
+              ))}
             </ul>
           </div>
         </div>
