bd568d0a5b8cb499eaa3e4161c45acdbe3d1d92d
Oleksii <alex.yurchenko@gmail.com>
2023-04-05 22:05:44 +0300

    server request

--- src/const.js
+++ src/const.js
@@ -1,2 +1,3 @@
 export const API_URI = "http://localhost:3024";
 export const POSTFIX = "/api/product";
+const SERVER_API = "https://cloudy-slash-rubidium.glitch.me/api/order";
--- src/store/order/orderSlice.js
+++ src/store/order/orderSlice.js
@@ -70,6 +70,10 @@ const orderSlice = createSlice({
         );
       }
     },
+    clearOrder: (state) => {
+      state.orderList = [];
+      state.orderGoods = [];
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -95,5 +99,5 @@ const orderSlice = createSlice({
   },
 });
 
-export const { addProduct, removeProduct } = orderSlice.actions;
+export const { addProduct, removeProduct, clearOrder } = orderSlice.actions;
 export default orderSlice.reducer;
